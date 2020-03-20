4d975af30eb2a2ec09defe51bc8bc61c2571a19b
jhuang429 <jhuang9240@gmail.com>
2020-03-20 12:27:55 -0400

    add search that links to USDA api

--- src/App.js
+++ src/App.js
@@ -1,36 +1,35 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Search from './components/Search';
 
 class App extends Component{
   
   state={
-    data: []
+    data: [],
+    Search: ""
   }
 
-  componentDidMount(){
-    fetch(`http://127.0.0.1:3000/search/pizza`)
+  // componentDidMount(){
+  //   fetch(`http://127.0.0.1:3000/search/pizza`)
+  //   .then(resp => resp.json())
+  //   .then(data => this.setState({ data }))
+  // }
+
+  handleSearchBar = (term) => {
+    fetch(`http://127.0.0.1:3000/search/${term}`)
     .then(resp => resp.json())
     .then(data => this.setState({ data }))
   }
-
-  // getFoods=()=>{
-  //   fetch(`http://127.0.0.1:3000/search/pizza`)
-  //     .then(resp => resp.json())
-  //     .then(data => this.setState({ data }))
-
-  // this.state.data.map(food => {return <p>{food.dataType}</p>})
-  // }
-
 
   
   render(){
 
     return (
       <div className="App">
-        <h1>Hi</h1>
-        {/* <button type="button" onClick={this.getFoods}>Search</button> */}
-        {this.state.data.map(food => {return <p>{food.dataType}</p>})}
+        <h1>Pantry Hero</h1>
+        <Search handleSearchBar={this.handleSearchBar}/>
+        {this.state.data.map(food => {return <p>{food.brandOwner}</p>})}
       </div>
     );
   }
