9a5245d65b108874a83b06d944a5cdb8075c3774
adam-xiao <adam.x.xiao@gmail.com>
2020-03-20 09:25:21 -0400

    almost back to front connect

--- src/App.js
+++ src/App.js
@@ -1,26 +1,39 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+  
+  state={
+    data: []
+  }
+
+  componentDidMount(){
+    fetch(`http://127.0.0.1:3000/search/pizza`)
+    .then(resp => resp.json())
+    .then(data => this.setState({ data }))
+  }
+
+  // getFoods=()=>{
+  //   fetch(`http://127.0.0.1:3000/search/pizza`)
+  //     .then(resp => resp.json())
+  //     .then(data => this.setState({ data }))
+
+  // this.state.data.map(food => {return <p>{food.dataType}</p>})
+  // }
+
+
+  
+  render(){
+
+    return (
+      <div className="App">
+        <h1>Hi</h1>
+        {/* <button type="button" onClick={this.getFoods}>Search</button> */}
+        {this.state.data.map(food => {return <p>{food.dataType}</p>})}
+      </div>
+    );
+  }
 }
 
 export default App;
