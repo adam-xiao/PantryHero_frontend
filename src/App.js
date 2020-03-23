e18582b9d86a6d89f28dcfa39d779f1bda0d4004
jhuang429 <jhuang9240@gmail.com>
2020-03-23 17:06:18 -0400

    cleaned up frontend with new foodcard

--- src/App.js
+++ src/App.js
@@ -2,20 +2,48 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Search from './components/Search';
+import FoodCard from './components/FoodCard';
+import { NavLink } from 'react-router-dom';
 
 class App extends Component {
 
   state = {
-    products: [],
-    Search: ""
+    data: [],
+    Search: "",
+    currentIng: ""
   }
 
   handleSearchBar = (term) => {
-    fetch(`http://127.0.0.1:3000/product/search/${term}`).catch((error) => console.error(error))
+    fetch(`http://127.0.0.1:3000/product/search/${term}`)
       .then(resp => resp.json())
-      .then(products => this.setState({ products }))
+      .then(data => this.parseData(data))
   }
 
+
+
+  parseData = (arr) => {
+    this.setState({
+      currentIng: {
+        id: arr[0].ndb_no,
+        food_name: arr[0].food_name,
+        nf_calories: arr[0].nf_calories,
+        nf_cholesterol: arr[0].nf_cholesterol,
+        nf_dietary_fiber: arr[0].nf_dietary_fiber,
+        nf_p: arr[0].nf_p,
+        nf_potassium: arr[0].nf_potassium,
+        nf_protein: arr[0].nf_protein,
+        nf_saturated_fat: arr[0].nf_saturated_fat,
+        nf_sodium: arr[0].nf_sodium,
+        nf_sugars: arr[0].nf_sugars,
+        nf_total_carbohydrate: arr[0].nf_total_carbohydrate,
+        nf_total_fat: arr[0].nf_total_fat,
+        img_url: arr[0].photo.highres
+      }
+    })
+  }
+
+
+  
 
   render() {
 
@@ -23,38 +51,10 @@ class App extends Component {
       <div className="App">
         <h1>Pantry Hero</h1>
         <Search handleSearchBar={this.handleSearchBar} />
-
-        {this.state.products.map(food => (
-          <div key={food.food_name}>
-            <h1>{food.food_name}</h1>
-            <img src={food.photo.highres} />
-                <div> Calories: {food.nf_calories && food.nf_calories} </div>
-                <div> Cholesterol: {food.nf_cholesterol && food.nf_cholesterol} </div>
-                <div> Dietary Fiber: {food.nf_dietary_fiber && food.nf_dietary_fiber} </div>
-                <div> P: {food.nf_p && food.nf_p} </div>
-                <div> Potassium: {food.nf_potassium && food.nf_potassium} </div>
-                <div> Protien: {food.nf_protein && food.nf_protein} </div>
-                <div> Saturated Fat: {food.nf_saturated_fat && food.nf_saturated_fat} </div>
-                <div> Sodium: {food.nf_sodium && food.nf_sodium} </div>
-                <div> Sugar: {food.nf_sugars && food.nf_sugars} </div>
-                <div> Carbohydrates: {food.nf_total_carbohydrate && food.nf_total_carbohydrate} </div>
-                <div> Total Fat: {food.nf_total_fat && food.nf_total_fat} </div>
-                
-            <button>Add to Pantry</button>
-          </div>
-        )
-        )}
-
-        {/* {this.state.products.map(food => (
-          <div key={food.id}>
-            <p>{food.title}</p>
-            <img src={food.image} />
-          </div>
-        )
-        )} */}
+        {this.state.currentIng && <FoodCard food={this.state.currentIng} />}
       </div>
     );
-  }x
+  }
 }
 
 export default App;
---
+++ src/components/FoodCard.js
@@ -0,0 +1,56 @@
+import React, { Component, useState } from 'react';
+
+
+
+
+
+function FoodCard(props) {
+
+    const handleAdd = () => {
+        fetch(`http://127.0.0.1:3000/ingredient/create`,
+        {
+          method: 'post',
+          body: JSON.stringify(props.food)
+        }
+        )
+      }
+
+    const { id,
+        food_name,
+        nf_calories,
+        nf_cholesterol,
+        nf_dietary_fiber,
+        nf_p,
+        nf_potassium,
+        nf_protein,
+        nf_saturated_fat,
+        nf_sodium,
+        nf_sugars,
+        nf_total_carbohydrate,
+        nf_total_fat,
+        img_url} = props.food
+
+    return (
+        <div key={id}>
+            <h1>{food_name}</h1>
+            <img src={img_url} />
+            <div> Calories: {nf_calories} </div>
+            <div> Cholesterol: {nf_cholesterol} </div>
+            <div> Dietary Fiber: {nf_dietary_fiber} </div>
+            <div> P: {nf_p} </div>
+            <div> Potassium: {nf_potassium} </div>
+            <div> Protien: {nf_protein} </div>
+            <div> Saturated Fat: {nf_saturated_fat} </div>
+            <div> Sodium: {nf_sodium} </div>
+            <div> Sugar: {nf_sugars} </div>
+            <div> Carbohydrates: {nf_total_carbohydrate} </div>
+            <div> Total Fat: {nf_total_fat} </div>
+
+
+            <button onClick={handleAdd}>Add to Pantry</button>
+        </div>
+    )
+}
+
+export default FoodCard
+
