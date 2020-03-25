d39daa99e5d0b96e35e03f473fb46547d84a81df
jhuang429 <jhuang9240@gmail.com>
2020-03-25 15:16:52 -0400

    fixed rerender problem + add qty to search component

--- src/containers/Title.js
+++ src/containers/Title.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
 
 function Title (props) {
+
+const handleButton = () => {
+    localStorage.token ? props.history.push("/home"): props.history.push("/login")
+}
+
 return(
     <div id="title">
         <h1 className="titleText">Welcome to</h1>
         <h1 className="titleText">PANTRY HERO</h1>
-        <button onClick={()=>props.history.push("/login")} className="btn btn-primary btn-lg">Get Started</button>
+        <button onClick={handleButton} className="btn btn-primary btn-lg">Get Started</button>
     </div>
 )
 }
